81cb85bd3d93951d213d71b5a5dc6a5f4395d3ec
Pedro Henrique B. Peres <pedrohperescode@gmail.com>
2022-08-17 21:41:08 -0300

    Add Cart Page

---
+++ lib/components/cart_itemwidget.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../models/cart_item.dart';
+
+class CartItemWidget extends StatelessWidget {
+  final CartItem cartItem;
+
+  const CartItemWidget({Key? key, required this.cartItem}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(cartItem.name);
+  }
+}
--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,16 +1,57 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/components/cart_itemwidget.dart';
+
+import '../models/cart.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final Cart cart = Provider.of(context);
+    final items = cart.items.values.toList();
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Carrinho'),
       ),
+      body: Column(
+        children: [
+          Card(
+            margin: const EdgeInsets.all(25),
+            child: Padding(padding: const EdgeInsets.all(10),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text('Total',
+                  style: TextStyle(fontSize: 20, ),
+                  ),
+                  SizedBox(width: 20),
+                  Chip(label: Text("R\$${cart.totalAmount}", style: TextStyle(color: Colors.white),),
+                  backgroundColor: Theme.of(context).colorScheme.primary,),
+                  Spacer(),
+                  TextButton(
+                    child: Text("COMPRAR"),
+                    style: TextButton.styleFrom(
+                      textStyle: TextStyle(color: Theme.of(context).colorScheme.primary)
+                    ),
+                    onPressed: (){},
+                  )
+                ],
+              ),
+            )
+          ),
+          Expanded(
+            child: ListView.builder(
+              itemCount: items.length,
+              itemBuilder: (ctx, index) => CartItemWidget(cartItem: items[index]),
+            ),
+          ),
+        ],
+      )
     );
   }
 }
